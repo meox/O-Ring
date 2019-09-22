d321b3844cf44010f381f8c5b276c9ce24884e63
Gian Lorenzo Meocci <glmeocci@gmail.com>
2019-09-22 09:30:51 +0200

    Adding first Go version

---
+++ golang/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+type chanT chan int
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("./ring N:<number of process> M:<iteration>")
+		os.Exit(1)
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("cannot parse number of process")
+	}
+
+	m, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("cannot parse number of iterations")
+	}
+
+	fmt.Println("Creating Ring")
+	t0 := time.Now()
+	s, e := createRing(n)
+	t1 := time.Now()
+	fmt.Printf("Ring created in: %d ms\n", t1.Sub(t0).Milliseconds())
+
+	for i := 0; i < m; i++ {
+		s <- i
+		_ = <- e
+	}
+	t2 := time.Now()
+
+	fmt.Printf(
+		"runned in: %d ms, tot: %dms\n",
+		t2.Sub(t1).Milliseconds(),
+		t2.Sub(t0).Milliseconds(),
+	)
+}
+
+func createRing(n int) (chanT, chanT) {
+	startChannel := make(chanT)
+
+	node := func(src, dst chanT) {
+		for {
+			msg := <- src
+			dst <- msg
+		}
+	}
+
+	src := startChannel
+	var dst chanT
+	for i := 0; i < n; i++ {
+		dst = make(chanT)
+		go node(src, dst)
+		src = dst
+	}
+
+	return startChannel, dst
+}
