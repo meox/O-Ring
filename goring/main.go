ee67581deec94195ffb9900b64b83194ef97bcfb
Gian Lorenzo Meocci <glmeocci@gmail.com>
2019-10-17 15:53:00 +0200

    make it compatible with the guidelines

--- goring/main.go
+++ goring/main.go
@@ -25,23 +25,24 @@ func main() {
 		fmt.Println("cannot parse number of trips")
 	}
 
-	fmt.Println("Creating Ring")
+	//fmt.Println("Creating Ring")
 	t0 := time.Now()
 	s, e := createRing(n)
-	t1 := time.Now()
-	fmt.Printf("Ring created in: %d ms\n", t1.Sub(t0).Milliseconds())
+	//t1 := time.Now()
+	//fmt.Printf("Ring created in: %d ms\n", t1.Sub(t0).Milliseconds())
 
 	for i := 0; i < m; i++ {
 		s <- i
 		_ = <-e
 	}
-	t2 := time.Now()
+	//t2 := time.Now()
 
-	fmt.Printf(
-		"runned in: %d ms, tot: %dms\n",
-		t2.Sub(t1).Milliseconds(),
-		t2.Sub(t0).Milliseconds(),
-	)
+	//fmt.Printf(
+	//	"runned in: %d ms, tot: %dms\n",
+	//	t2.Sub(t1).Milliseconds(),
+	//	t2.Sub(t0).Milliseconds(),
+	//)
+	fmt.Printf("%d\n", time.Since(t0).Milliseconds())
 }
 
 func createRing(n int) (chanT, chanT) {
